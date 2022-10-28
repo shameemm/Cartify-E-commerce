3816084d48e06970789198871e4b4049d5bf2558
shameemm <mandayappuramshameem@gmail.com>
2022-10-28 18:47:17 +0530

    updates on view_product

--- users/views.py
+++ users/views.py
@@ -273,9 +273,18 @@ def view_product(request):
     print(product)
     prdct = Product.objects.filter(id=id)
     print(prdct)
+    off=''
+    offc=''
+    if Offers.objects.filter(product_id=id).exists():
+        off=Offers.objects.get(product_id=id)
+        print("off",off.offer)
+    if Offers.objects.filter(category=product.category).exists():
+        offc = Offers.objects.get(category=product.category)
+        print("offc",offc.offer)
     images = Images.objects.filter(product=prdct[0].id)
     offers = Offers.objects.all()
     for offer in offers:
+        
         if offer.product == prdct[0]:
             # print("offer=",offer.name)
             for ofr in offers:
@@ -284,9 +293,10 @@ def view_product(request):
                     print("ofr=",ofr.name)
                     if ofr.offer<offer.offer:
                         print("offer",offer.offer)
-                        return render(request, 'user/view_product.html',{'product': product, 'images': images, 'offer': offer})
+                        
+                        return render(request, 'user/view_product.html',{'product': product, 'images': images, 'offer': offer, 'offc':offc, 'off': off})
                     else:
-                        return render(request, 'user/view_product.html',{'product': product, 'images': images, 'offer':ofr})
+                        return render(request, 'user/view_product.html',{'product': product, 'images': images, 'offer':ofr,  'offc':offc, 'off': off})
                 # else:
                     
         else: 
@@ -294,7 +304,7 @@ def view_product(request):
                 # print("offer=",offer.name)
                 if ofr.category == product.category:
                     print("elseofr=",ofr.name)
-                    return render(request, 'user/view_product.html',{'product': product, 'images': images, 'offer':ofr})
+                    return render(request, 'user/view_product.html',{'product': product, 'images': images, 'offer':ofr, 'offc':offc, 'off': off})
                         
                         
                     
