a59178ced68241e9e8725f589b2c0bd3932c776e
shameemm <mandayappuramshameem@gmail.com>
2022-11-02 13:46:56 +0530

    allowed hosts

--- cart/settings.py
+++ cart/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-g^6q*gpn!8jy6fuuhjkb#3(v$@c_w&j2_2fg9-tv%tjr32#jwq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['35.77.103.8','localhost']
 
 
 # Application definition
